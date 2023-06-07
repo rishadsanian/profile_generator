9f076a3cede624dda3c62db6ed1f1e4850095f26
Rishad Alam <rishad.s.a@gmail.com>
2023-06-07 01:00:52 -0400

    refactored using recursion

--- survey.js
+++ survey.js
@@ -5,58 +5,39 @@ const rl = readline.createInterface({
   output: process.stdout,
 });
 
-rl.question(`What's your name? Nicknames are also acceptable :) `, (name) => {
-  console.log(`Name: ${name}`);
-  rl.question(`What's an activity you like doing? `, (activity) => {
-    console.log(`Favorite Activity: ${activity}`);
-    rl.question(`What kind of music do you like listening to? `, (music) => {
-      console.log(`Favorite Music: ${music}`);
-      rl.question(
-        `Which meal is your favorite (e.g., dinner, brunch, etc.)? `,
-        (meal) => {
-          console.log(`Favorite Meal: ${meal}`);
-          rl.question(
-            `What's your favorite thing to eat for that meal? `,
-            (food) => {
-              console.log(`Favorite Food for ${meal}: ${food}`);
-              rl.question(
-                `Which sport is your absolute favorite? `,
-                (sport) => {
-                  console.log(`Favorite Sport: ${sport}`);
-                  rl.question(
-                    `What is your superpower? In a few words, tell us what you are amazing at! `,
-                    (superpower) => {
-                      console.log(`Superpower: ${superpower}`);
 
-                      console.log(
-                        name +
-                          " loves listening to " +
-                          music +
-                          ", devouring " +
-                          food +
-                          " for " +
-                          meal +
-                          ", prefers " +
-                          sport +
-                          " over any other sport, and is amazing at " +
-                          superpower +
-                          "."
-                      );
-                      //Devani loves listening to Ludovico Einaudi while coding, devouring Yak Momos for brunch, prefers Tennis over any other sport, and is amazing at dropping mad puns at inopportune times.
 
-                      rl.close();
-                    }
-                  );
-                }
-              );
-            }
-          );
-        }
-      );
+const askQuestion = (questions, index = 0, answers = []) => {
+  if (index < questions.length) {
+    rl.question(questions[index], (answer) => {
+      answers.push(answer);
+      askQuestion(questions, index + 1, answers);
     });
-  });
-});
+  } else {
+    displayResults(answers);
+    rl.close();
+  }
+};
 
+const displayResults = (answers) => {
+  const [name, activity, music, meal, food, sport, superpower] = answers;
+  console.log(
+    `${name} loves listening to ${music} while ${activity}, devouring ${food} for ${meal}, prefers ${sport} over any other sport, and is amazing at ${superpower}.`
+  );
+};
+
+
+const questions = [
+  `What's your name? Nicknames are also acceptable :) `,
+  `What's an activity you like doing? `,
+  `What kind of music do you like listening to? `,
+  `Which meal is your favorite (e.g., dinner, brunch, etc.)? `,
+  `What's your favorite thing to eat for that meal? `,
+  `Which sport is your absolute favorite? `,
+  `What is your superpower? In a few words, tell us what you are amazing at! `,
+];
+
+askQuestion(questions);
 // What's your name? Nicknames are also acceptable :)
 // What's an activity you like doing?
 // What do you listen to while doing that?
